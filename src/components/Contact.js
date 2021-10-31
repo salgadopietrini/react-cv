175896dd946e7bf3f4b2c4c5a26ff2394ebab1c5
ManuelSalgado <manuel.salgado@bootcamp-upgrade.com>
2021-10-31 01:13:36 +0000

    add contacts info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,63 @@
 import React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import Link from "@mui/material/Link";
 
 export default function Contacts() {
-  return <div>Contacts</div>;
+  const contacts = [
+    {
+      type: "email",
+      data: "salgadopietrini7@gmail.com",
+      logo: "logo",
+    },
+    {
+      type: "phone",
+      data: "(+351) 910274805",
+      logo: "logo",
+    },
+    {
+      type: "github",
+      data: "https://github.com/salgadopietrini",
+      logo: "logo",
+    },
+    {
+      type: "linkedin",
+      data: "https://www.linkedin.com/in/msalgadopietrini/",
+      logo: "logo",
+    },
+  ];
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <List sx={{ width: "100%", maxWidth: 400, bgcolor: "background.paper" }}>
+        {contacts.map((elem, index) => (
+          <div key={index}>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar alt="Company Logo" src={elem.logo} />
+              </ListItemAvatar>
+              <ListItemText
+                primary={
+                  <Link color="inherite" underline="hover">
+                    {elem.data}
+                  </Link>
+                }
+              />
+            </ListItem>
+            <Divider variant="inset" component="li" />
+          </div>
+        ))}
+      </List>
+    </div>
+  );
 }
